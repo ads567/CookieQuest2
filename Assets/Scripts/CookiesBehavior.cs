cc74de63d4edebfa093e26e19bfd4c2dbc48e6b2
Adam Soltys <ads576@mail.usask.ca>
2020-05-28 12:11:03 -0600

    Can click plate of cookies, and it rotates each time

---
+++ Assets/Scripts/CookiesBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookiesBehavior : MonoBehaviour
+{
+
+    public bool broken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        broken = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BreakCookies()
+    {
+        if(broken == false)
+        {
+            broken = true;
+        }
+        transform.Rotate(Vector3.forward * Random.Range(15.0f, 180.0f));
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse click on cookies detected");
+        BreakCookies();
+    }
+}
