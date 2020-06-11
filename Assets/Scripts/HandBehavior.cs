1d2dda81f4f9237a26dbc525764bf404aa4a31ab
Adam Soltys <adam.soltys@usask.ca>
2020-06-10 22:04:28 -0600

    Hand moves towards cookies.

---
+++ Assets/Scripts/HandBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandBehavior : MonoBehaviour
+
+{
+    GameObject cookies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cookies = GameObject.FindWithTag("Cookies");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cookies != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, cookies.transform.position, 0.015f);
+            transform.rotation = Quaternion.SetLookRotation(cookies.transform.position, Vector3.up);
+        }
+    }
+}
