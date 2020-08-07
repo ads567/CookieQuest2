2f4af3a1bb467e1ba5d6611b83d3912d6314faba
Adam Soltys <adam.soltys@usask.ca>
2020-08-06 21:39:19 -0600

    Commenting code

--- Assets/Scripts/CookiesBehavior.cs
+++ Assets/Scripts/CookiesBehavior.cs
@@ -17,6 +17,7 @@ public class CookiesBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if the cookies are currently held, and if they are, stay with the holder.
         if(holder != null)
         {
             transform.position = holder.transform.position;
@@ -24,26 +25,39 @@ public class CookiesBehavior : MonoBehaviour
     }
 
     void BreakCookies()
+        /**
+         * This function is called when the plate is clicked on. It ends the game. 
+         */
     {
         if(broken == false)
         {
             broken = true;
         }
+        // Randomly rotate the shattered plate.
         transform.Rotate(Vector3.forward * Random.Range(15.0f, 180.0f));
     }
 
     private void OnMouseDown()
+        /**
+         * Function that checks if the cookies have been clicked on. Calls BreakCookies()
+         */
     {
         Debug.Log("Mouse click on cookies detected");
         BreakCookies();
     }
 
     public void SetHolder(GameObject newHolder)
+        /**
+         * Sets the current holder of the plate. Called from the HandBehavior script when a hand gets close enough.
+         */
     {
         holder = newHolder;
     }
 
     public void ResetHolder()
+        /**
+         * Resets the holder variable
+         */
     {
         holder = null;
     }
--- Assets/Scripts/HandBehavior.cs
+++ Assets/Scripts/HandBehavior.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class HandBehavior : MonoBehaviour
 
 {
+    // Where the hand came in from. Used when making the hand retreat with the cookies.
     Vector3 spawn;
+
+    // The plate of cookies, and its behavior script
     GameObject cookies;
     CookiesBehavior cb;
-    Quaternion target;
-    Vector3 direction;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Set the cookies variable and its behavior script, and set initial rotation and spawn point. 
         cookies = GameObject.FindWithTag("Cookies");
         cb = cookies.gameObject.GetComponent<CookiesBehavior>();
         transform.rotation = Quaternion.LookRotation(Vector3.forward, cookies.transform.position - transform.position);
@@ -25,6 +27,7 @@ public class HandBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Make sure the cookies exist, then check if this is hoding them. If it is, move back towards the spawn. If it isn't, move towards the cookies. 
         if (cookies != null)
         {
             if(cb.holder == gameObject)
@@ -36,14 +39,13 @@ public class HandBehavior : MonoBehaviour
                 transform.position = Vector3.MoveTowards(transform.position, cookies.transform.position, 0.011f);
             }
             
+            // Rotate to face the cookies. 
             transform.rotation = Quaternion.LookRotation(Vector3.forward, cookies.transform.position - transform.position);
 
-
-
+            // If we are close enough to the cookies and they don't have a holder, become the holder. 
             if(Vector3.Distance(transform.position, cookies.transform.position) < 0.1f & cb.holder == null)
             {
                 cb.SetHolder(gameObject);
-                direction *= -1.0f;
             }
         }
     }
