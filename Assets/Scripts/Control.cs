d41745a2381036e98b92132d0b86da4b5216fdbb
Adam Soltys <adam.soltys@usask.ca>
2020-08-08 20:59:20 -0600

    Commenting

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -23,13 +23,27 @@ public class Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        /*
+         * Because of the unpredictable nature of overlapping colliders on the z axis, hit detection must be done carefully and deliberately. 
+         */
+
+        // Check for a mouse click
         if (Input.GetMouseButtonDown(0))
         {
+            // Create an array for all hits for the raycast at mouse position
             RaycastHit2D[] hit = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            // List of colliders, pulled from the hits (mostly just for convenience)
             List<Collider2D> colliders = new List<Collider2D>();
+
+            // The cookies behavior script, from which we'll need the current holder of the cookies
             CookiesBehavior cb = cookies.GetComponent<CookiesBehavior>();
+
+            // The highest index found for all colliders at the click position. The holder of the cookies is prioritized, then the highest hand after that and so on. 
             int highestIndex = 0;
 
+            // Iterate through the RaycastHits and add their colliders to a list. Compare to the current highest index and update if it's higher. 
             foreach (RaycastHit2D h in hit)
             {
                 colliders.Add(h.collider);
@@ -39,10 +53,13 @@ public class Control : MonoBehaviour
                 }
             }
 
+            // If the cookies are the only thing that was clicked, destroy them
             if (colliders.Contains(cookies) & colliders.Count == 1)
             {
                 cb.BreakCookies();
             }
+
+            // If the cookies have a holder and it was in the clicked colliders, destroy it. 
             else if (cb.holder != null)
             {
                 if (colliders.Contains(cb.holder.GetComponent<Collider2D>()))
@@ -50,6 +67,8 @@ public class Control : MonoBehaviour
                     cb.holder.GetComponent<HandBehavior>().SlapHand();
                 }
             }
+
+            // Otherwise, destroy the "highest" hand
             else
             {
                 if(colliders.Count > 0)
@@ -57,62 +76,6 @@ public class Control : MonoBehaviour
                     colliders[highestIndex].gameObject.GetComponent<HandBehavior>().SlapHand();
                 }
             }
-
-
-
-
-
-
-
-
-
-
-
-            /*
-            // Iterate through each object under the mouse
-            foreach(RaycastHit2D h in hit)
-            {
-                int highestIndex = 0;
-                // If the object clicked was the cookies, break them
-                if (h.collider.CompareTag("Cookies") & hit.Length == 1)
-                {
-                    GameObject.FindWithTag("Cookies").GetComponent<CookiesBehavior>().BreakCookies();
-                }
-
-                // If the object being checked is a hand
-                else if (h.collider.CompareTag("Hand"))
-                {
-                    // If the hand is holding the cookies, slap that hand
-                    if(h.collider.gameObject == GameObject.FindWithTag("Cookies").GetComponent<CookiesBehavior>().holder)
-                    {
-                        h.collider.gameObject.GetComponent<HandBehavior>().SlapHand();
-                    }
-
-                    // Otherwise, check if it is the highest hand. If it is, update the highest hand index
-                    else
-                    {
-                        if(System.Array.IndexOf(hit, h.collider.gameObject) > highestIndex)
-                        {
-                            highestIndex = System.Array.IndexOf(hit, h);
-
-                            // If it's the last collider, delete the one with the highest index
-                            if(System.Array.IndexOf(hit, h) == hit.Length - 1)
-                            {
-                                Debug.Log("Made it here");
-
-                                foreach (GameObject g in objects)
-                                {
-                                    if (g == hit[highestIndex].collider.gameObject)
-                                    {                                      
-                                        g.GetComponent<HandBehavior>().SlapHand();
-                                    }
-                                }
-                            }
-                        }                       
-                    }
-                }
-            }
-            */
         }
     }
 }
